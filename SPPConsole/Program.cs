86f89e72efb16d9f8ea20bc077f3c429ec3f87cc
Cole Hayden <colehayden@Coles-Mac-mini.local>
2024-02-23 11:29:02 -0600

    removing comments

--- SPPConsole/Program.cs
+++ SPPConsole/Program.cs
@@ -30,38 +30,3 @@ while (tryAgain == true)
     }
     while (tryAgain == true);
 }
-
-/*
-bool tryAgain = true;
-ApiHelper.InitializeClient();
-
-while (tryAgain == true)
-{
-    DogModel newDog = null;
-    Console.WriteLine("\nPlease enter a dog breed to fetch a image.");
-    string? dogBreed = Console.ReadLine();
-
-    if (await client.IsDogCached(dogBreed) == false)
-    {
-        newDog = await client.CheckForKindOfBreedAndSelectDog(dogBreed);
-        if (newDog != null)
-        {
-            await client.InsertDogImageAndBreed(newDog);
-        }
-    }
-    do
-    {
-        Console.WriteLine(@"Would you like to try again?  Please type in Y or N");
-        ConsoleKey keyInfo = Console.ReadKey().Key;
-        if (keyInfo == ConsoleKey.N)
-        {
-            Console.WriteLine("\nThank you for using the application.  Goodbye!");
-            return;
-        }
-        else if (keyInfo == ConsoleKey.Y)
-        {
-            break;
-        }
-    }
-    while (tryAgain == true);
-}*/
